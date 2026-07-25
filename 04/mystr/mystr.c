28d07edf6a6a6d71d240dd2deb1c0920a14f881a
leadbraw <noah.dangerfield@gmail.com>
2026-07-24 17:01:15 -0700

    scaffolding for module 04 project

---
+++ 04/mystr/mystr.c
@@ -0,0 +1,30 @@
+#include "mystr.h"
+
+size_t my_strlen(const char *s) {
+    const char* c = s;
+    while (*c) {
+        c++;
+    }
+    return (c - s);
+}
+
+
+char* my_strcpy(char* dst, const char* src) {
+    
+}
+
+int my_strcmp(const char* a, const char* b) { 
+
+}
+
+char* my_strchr(const char* s, const char* b) {
+    
+}
+
+void my_strrev(char* s) {
+    
+}
+
+size_t my_split(char* s, char sep, char* parts[], size_t max_parts){
+    
+}
---
+++ 04/mystr/mystr.h
@@ -0,0 +1,13 @@
+#ifndef MYSTR_H
+#define MYSTR_H
+
+#include <stddef.h>
+
+size_t my_strlen(const char *s);
+char* my_strcpy(char* dst, const char* src);
+int my_strcmp(const char* a, const char* b);
+char* my_strchr(const char* s, const char* b);
+void my_strrev(char* s);
+size_t my_split(char* s, char sep, char* parts[], size_t max_parts);
+
+#endif
