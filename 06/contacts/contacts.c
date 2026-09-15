05b86f46372fd5e70650d582d9f916c1460a347f
leadbraw <noah.dangerfield@gmail.com>
2026-09-14 19:05:49 -0700

    06: contacts data layer done

--- 06/contacts/contacts.c
+++ 06/contacts/contacts.c
@@ -1,8 +1,99 @@
 #include "contacts.h"
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
 
 struct Contacts {
     Contact* data;
     size_t len;
     size_t cap;
 };
+
+// utility function used by con_find()
+bool starts_with(const char *str, const char *pre) {
+    while (*pre) {
+        if (*pre != *str) {
+            return false;
+        }
+        str++;
+        pre++;
+    }
+    return true;
+}
+
+static int con_grow(Contacts* c) {
+    Contact* tmp = realloc(c->data, c->cap * 2 * sizeof *c->data);
+    if (!tmp) {
+        return -1; 
+    }
+    else { 
+        c->data = tmp; 
+        c->cap *= 2;
+        return 0;
+    }
+}
+
+// here, pop is just used internally. as opposed to vec where it's exposed
+void con_pop(Contacts* c) {
+    if (c->len == 0) return;
+    c->len -= 1; // we'll just overwrite it on the next push
+}
+
+Contacts* con_new(void) {
+    Contacts* c = malloc(sizeof(*c));
+    if (!c) return NULL;
+    c->data = malloc(8 * sizeof *c->data);
+    if (!c->data) {
+        free(c);
+        return NULL;
+    }
+    c->len = 0;
+    c->cap = 8;
+    return c;    
+}
+
+int con_push(Contacts* c, Contact* con) {
+    if (c->len == c->cap) {
+        assert(con_grow(c) == 0);
+    }
+    c->data[c->len++] = *con; //idk if this assignment is right re: dereferencing
+    return 0;
+}
+
+Contact* con_get(const Contacts* c, size_t i) {
+    assert(i < c->len);
+    return &c->data[i];    
+}
+
+size_t con_len(const Contacts* c) {
+    return c->len;
+}
+
+void con_delete(Contacts* c, size_t i) {
+    if (i >= c->len) return;
+    if (i == c->len - 1) { con_pop(c); return; }
+    
+    memmove(c->data + i, c->data + i + 1, (c->len - i - 1) * sizeof *c->data);
+    c->len -= 1;
+    return;
+}
+
+// prefix, return first match. only checks names (not email)
+Contact* con_find(Contacts* c, const char* str) {
+    if (strlen(str) > 25) { return NULL; } // returning NULL on failure and error...
+
+    for (size_t i = 0; i < c->len; i++) {
+        if (starts_with(con_get(c, i)->name, str)) {
+            return con_get(c, i);
+        }
+    }
+    return NULL;
+}
+
+void con_free(Contacts* c) {
+    if (!c) return;
+    
+    free(c->data);
+    free(c);
+}
--- 06/contacts/contacts.h
+++ 06/contacts/contacts.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 #ifndef CONTACTS_H
 #define CONTACTS_H
 
@@ -8,5 +10,13 @@ typedef struct {
 } Contact;
 
 typedef struct Contacts Contacts;
+
+Contacts* con_new(void);
+int con_push(Contacts* c, Contact* con);
+Contact* con_get(const Contacts* c, size_t i);
+size_t con_len(const Contacts* c);
+void con_delete(Contacts* c, size_t i);
+Contact* con_find(Contacts* c, const char* str);
+void con_free(Contacts* c);
 
 #endif
